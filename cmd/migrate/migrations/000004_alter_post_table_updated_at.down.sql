fa7006278ba59f11e3dc773f3cfdc6abd1257a2f
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-12-02 19:04:07 -0400

    fix: change db's default for prevent exception on null values
    - "updatedAt" by default has same value of "createdAt"

---
+++ cmd/migrate/migrations/000004_alter_post_table_updated_at.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE posts
+  ALTER COLUMN updated_at DROP NOT NULL,
+  ALTER COLUMN updated_at DROP DEFAULT;
---
+++ cmd/migrate/migrations/000004_alter_post_table_updated_at.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE posts
+  ALTER COLUMN updated_at SET NOT NULL,
+  ALTER COLUMN updated_at SET DEFAULT now();
