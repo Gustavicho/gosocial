a033fd849e45ca6b29e00f46280aec147fcc1627
Gustavo Oliveira <gustavoooliveiradecarvalho@gmail.com>
2024-12-04 10:52:07 -0400

    feat: add error parten with funcs

---
+++ cmd/api/error.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Internal server error: '[%s] %s' error: %s", r.Method, r.URL.Path, err)
+
+	writeJSONError(w, http.StatusInternalServerError, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Not found: '[%s] %s' error: %s", r.Method, r.URL.Path, err)
+
+	writeJSONError(w, http.StatusNotFound, err.Error())
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Bad request: '[%s] %s' error: %s", r.Method, r.URL.Path, err)
+
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
--- cmd/api/post.go
+++ cmd/api/post.go
@@ -20,7 +20,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	var payload CreatePostPayload
 
 	if err := readJSON(w, r, &payload); err != nil {
-		writeJSONError(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
@@ -38,7 +38,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 
 	err := app.store.Posts.Create(ctx, post)
 	if err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 
@@ -50,7 +50,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	postIDParam := chi.URLParam(r, "id")
 	postID, err := strconv.ParseUint(postIDParam, 10, 64)
 	if err != nil {
-		writeJSON(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
@@ -61,9 +61,9 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		switch {
 		case errors.Is(err, store.ErrNotFound):
-			writeJSONError(w, http.StatusNotFound, err.Error())
+			app.notFoundResponse(w, r, err)
 		default:
-			writeJSONError(w, http.StatusInternalServerError, err.Error())
+			app.internalServerError(w, r, err)
 		}
 
 		return
